8b40141d8cca04efe034815646eb3e4518cf8699
Cam McInnes <camtmcinnes@gmail.com>
2023-11-16 18:33:31 -0800

    Create redundant page for organization and individual account creation

---
+++ project/frontend/src/components/CreateAccount.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import '../App.css';
+import Login from "../pages/Login";
+
+function CreateAccount() {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    return (
+        <div>
+            <h2>Create an Account</h2>
+            <div className={'create'}>
+                <label>Email:
+                    <input
+                        type='email'
+                        name='insertEmail'
+                        placeholder='enter email here'
+                        maxLength={50}
+                        value={username}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </label>
+                <label>Username:
+                    <input
+                        type='text'
+                        name='insertUsername'
+                        placeholder='enter username here'
+                        maxLength={50}
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                </label>
+                <label>Password:
+                    <input
+                        type='password'
+                        name='insertPass'
+                        placeholder='1234'
+                        maxLength={50}
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </label>
+                <label> Re-enter Password:
+                    <input
+                        type='password'
+                        name='insertPass2'
+                        placeholder='1234'
+                        maxLength={50}
+                        value={password2}
+                        onChange={e => setPassword2(e.target.value)}
+                    />
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default CreateAccount;
---
+++ project/frontend/src/pages/CreateIndividual.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../App.css';
+import CreateAccount from '../components/CreateAccount'
+
+function CreateIndividual() {
+    return(
+        <CreateAccount />
+    )
+}
