dc545bdd0e1ca2587f2e7e90a52565858ff20d7c
Cam McInnes <camtmcinnes@gmail.com>
2023-11-16 18:18:54 -0800

    got rid of redundancy in app.js

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -4,18 +4,18 @@ import Welcome from "./pages/Welcome";
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
-function App() {
- // const [account, setAccount] = useState({type: null});
-
-  // useEffect(() => {
-  // }, [account]);
-  return (
-    // <div className="App">
-    //   {account.type === null ? (<Welcome/>) :
-    //     (<Login setAccount={setAccount}/>)}
-    // </div>
-      <Welcome />
-  );
-}
+// function App() {
+//  // const [account, setAccount] = useState({type: null});
+//
+//   // useEffect(() => {
+//   // }, [account]);
+//   return (
+//     // <div className="App">
+//     //   {account.type === null ? (<Welcome/>) :
+//     //     (<Login setAccount={setAccount}/>)}
+//     // </div>
+//
+//   );
+// }
 
 export default App;
