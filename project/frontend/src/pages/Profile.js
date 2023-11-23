d7e4acc9207c3af711063ebd8dfc2acee1febe51
Cam McInnes <camtmcinnes@gmail.com>
2023-11-23 11:21:17 -0800

    add frontend for inserting a payment method

--- project/frontend/src/pages/Profile.js
+++ project/frontend/src/pages/Profile.js
@@ -4,9 +4,87 @@ import { useNavigate } from "react-router-dom";
 import { checkAuth } from "../utils";
 
 function Profile() {
-    return (
+    const URL = process.env.REACT_APP_URL;
+    const [accountType, setAccountType] = useState(null);
+    const navigate = useNavigate();
+    const [ccNumber, setCCNumber] = useState("");
+    const [ccv, setCCV] = useState("");
+    const [address, setAddress] = useState("");
+    const [postalCode, setPostalCode] = useState("");
 
-    );
+    useEffect(() => {
+        const isLoggedIn = async () => {
+            const token = localStorage.getItem("token");
+            const result = await checkAuth(token);
+            if (result === null) {
+                navigate("/login");
+            } else {
+                setAccountType(result)
+            }
+        }
+        isLoggedIn();
+    }, [])
+
+    const handlePaymentCreation = async () => {
+        try {
+
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
+    if (accountType === 'individual'){
+        return (
+            <div className={'payment'}>
+                <h1>Payment Information</h1>
+                <label>Credit Card Number:
+                    <input
+                        type={'text'}
+                        name={'insertCCNumber'}
+                        placeholder={'enter credit card number here'}
+                        maxLength={16}
+                        value={ccNumber}
+                        onChange = {e => setCCNumber(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <label>CCV:
+                    <input
+                        type={'text'}
+                        name={'insertCCV'}
+                        placeholder={'enter security code here'}
+                        maxLength={3}
+                        value={ccv}
+                        onChange = {e => setCCV(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <label>Address:
+                    <input
+                        type={'text'}
+                        name={'insertAddress'}
+                        placeholder={'enter address here'}
+                        maxLength={50}
+                        value={address}
+                        onChange = {e => setAddress(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <label>PostalCode:
+                    <input
+                        type={'text'}
+                        name={'insertPostalCode'}
+                        placeholder={'enter postalCode here'}
+                        maxLength={6}
+                        value={postalCode}
+                        onChange = {e => setPostalCode(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <button onClick={handlePaymentCreation} >Change Payment Info</button>
+            </div>
+        );
+    }
 }
 
 export default Profile;
