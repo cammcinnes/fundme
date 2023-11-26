234ba7243db2dc5954da211ff1945fd59c258745
Cam McInnes <camtmcinnes@gmail.com>
2023-11-26 11:41:22 -0800

    Fix query and routes to payment information

--- project/routes/payment.js
+++ project/routes/payment.js
@@ -11,26 +11,25 @@ const router = express.Router();
 
 router.post('/insert-payment', async(req, res) => {
     try {
-        const {ccNumber, cvv, address, postalCode, city, province} = req.body;
+        const {ccNumber, username, cvv, address, postalCode, city, province} = req.body;
         if (!ccNumber || !cvv || !address || !postalCode) {
             return res.status(400).json({
                 success: false,
                 error: "Credit Card Number, CVV, Address and PostalCode are required"
             });
         }
-        const ccNumberExists = await paymentQuery.ccExists(ccNumber);
+        const ccNumberExists = await paymentQuery.ccExists(ccNumber, username);
         if (ccNumberExists.length > 0) {
-            // add credit card to current account
             return res.status(400).json({
                 success: false,
-                error: "Credit Card is already associated with another account!"
+                error: "Credit Card is already attached to this account!"
             });
         }
         const postalCodeExists = await paymentQuery.postalCodeExists(postalCode);
         if (postalCodeExists.length === 0) {
             await paymentQuery.insertPostalInfo(postalCode, city, province);
         }
-        await paymentQuery.insertPaymentInfo(ccNumber, cvv, address, postalCode);
+        await paymentQuery.insertPaymentInfo(ccNumber, username, cvv, address, postalCode);
         return res.json({success: true});
     } catch (error) {
         return res.status(500).json({success: false, error: error.message});
